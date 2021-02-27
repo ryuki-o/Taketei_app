a04ba1779479c94c512cdd7c25b24cc3616bd9c5
ryuki-o <ap023otakeyama0503@outlook.jp>
2021-02-27 08:02:03 +0000

    static_pagesコントローラーを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210227073516) do
+ActiveRecord::Schema.define(version: 20210227075302) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20210227073516) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
